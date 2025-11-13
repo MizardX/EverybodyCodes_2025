4ffac408516b1133dc62639796d86977ac559218
MizardX <mizardx@gmail.com>
2025-11-13 08:39:00 +0100

    Day 8 - Clean up

--- src/day_08.rs
+++ src/day_08.rs
@@ -1,28 +1,15 @@
 use std::collections::HashMap;
 use std::num::ParseIntError;
 
-use thiserror::Error;
-
-#[derive(Debug, Error)]
-pub enum ParseError {
-    #[error("Syntax error")]
-    SyntaxError,
-    #[error(transparent)]
-    InvalidNumber(#[from] ParseIntError),
-}
-
 pub struct Day08;
 
 impl crate::Day for Day08 {
     type Input = Vec<u16>;
 
-    type ParseError = ParseError;
+    type ParseError = ParseIntError;
 
     fn parse(input: &str) -> Result<Self::Input, Self::ParseError> {
-        Ok(input
-            .split(',')
-            .map(str::parse)
-            .collect::<Result<Vec<_>, _>>()?)
+        input.split(',').map(str::parse).collect()
     }
 
     fn part_1(input: &Self::Input) -> u64 {
@@ -30,7 +17,7 @@ impl crate::Day for Day08 {
     }
 
     fn part_2(input: &Self::Input) -> u64 {
-        all_crossings(input, 256)
+        all_crossings(input)
     }
 
     fn part_3(input: &Self::Input) -> impl std::fmt::Display {
@@ -38,22 +25,20 @@ impl crate::Day for Day08 {
     }
 }
 
-fn center_crossings(input: &[u16], size: u16) -> u64 {
-    let mut prev = input[0];
+fn center_crossings(sequence: &[u16], nails: u16) -> u64 {
     let mut count = 0;
-    for &x in input {
-        if x.abs_diff(prev) == size / 2 {
+    for (&x, &y) in sequence.iter().zip(&sequence[1..]) {
+        if x.abs_diff(y) == nails / 2 {
             count += 1;
         }
-        prev = x;
     }
     count
 }
 
-fn all_crossings(input: &[u16], size: u16) -> u64 {
+fn all_crossings(sequence: &[u16]) -> u64 {
     let mut count = 0;
     let mut edges = vec![HashMap::<u16, u16>::new(); 256];
-    for (&x, &y) in input.iter().zip(&input[1..]) {
+    for (&x, &y) in sequence.iter().zip(&sequence[1..]) {
         for z1 in x.min(y) + 1..x.max(y) {
             for (&z2, &cnt) in &edges[z1 as usize - 1] {
                 if !(x.min(y)..=x.max(y)).contains(&z2) {
@@ -67,14 +52,14 @@ fn all_crossings(input: &[u16], size: u16) -> u64 {
     count
 }
 
-fn best_cut(input: &[u16], size: u16) -> u64 {
+fn best_cut(sequence: &[u16], nails: u16) -> u64 {
     let mut edges = vec![HashMap::<u16, u16>::new(); 256];
-    for (&x, &y) in input.iter().zip(&input[1..]) {
+    for (&x, &y) in sequence.iter().zip(&sequence[1..]) {
         *edges[x as usize - 1].entry(y).or_default() += 1;
         *edges[y as usize - 1].entry(x).or_default() += 1;
     }
     let mut max_count = 0;
-    for y in 2..=size {
+    for y in 2..=nails {
         for x in 1..y {
             let mut count = u64::from(edges[x as usize].get(&y).copied().unwrap_or_default());
             for z in x + 1..y {
@@ -95,7 +80,6 @@ mod tests {
     use crate::Day;
 
     use super::*;
-    // use test_case::test_case;
 
     const EXAMPLE1: &str = "1,5,2,6,8,4,1,7,3";
     const EXAMPLE2: &str = "1,5,2,6,8,4,1,7,3,5,7,8,2";
@@ -111,7 +95,7 @@ mod tests {
     #[test]
     fn test_part_2() {
         let input = Day08::parse(EXAMPLE2).unwrap();
-        let result = all_crossings(&input, 8);
+        let result = all_crossings(&input);
         assert_eq!(result, 21);
     }
 
